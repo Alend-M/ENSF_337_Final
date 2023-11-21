ae9226beaea646108527bb1736fc1bac344034c3
Alend-M <alend.maci@ucalgary.ca>
2023-11-21 16:24:45 -0700

    Wrote some class names

---
+++ Main.cpp
@@ -0,0 +1,34 @@
+#ifndef Flight_Manager
+#define Flight_Manager
+#include <vector>
+#include <string> 
+//*****NOTES*****
+//Use vector use for list
+//Use String C++ class for strings
+//Bonus mark if we use a link list  (10%)
+//*****ENDNOTE*****
+
+class Airline{
+
+};
+
+class Flight{
+
+};
+
+class Passanger{
+
+    private: 
+    char Fname; //First Name 
+    char LName; //Last Name 
+    //*Phone number make linked list
+    int ID; 
+};
+
+class Seat{
+
+    private:
+    int Row;    //Seat row 
+    int Column; //Seat column
+};
+#endif
