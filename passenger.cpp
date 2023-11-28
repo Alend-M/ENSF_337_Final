aa3358cbd1b8cda9a014a521e2c6e337d0eccad3
joshuamaher61 <144175356+joshuamaher61@users.noreply.github.com>
2023-11-28 15:32:01 -0700

    Update passenger.cpp

--- passenger.cpp
+++ passenger.cpp
@@ -1,1 +1,13 @@
 #include "passenger.h"
+
+//displaypassengerlist();
+//pressEnter(); to pause program
+
+//addpassenger(f);
+//break;
+
+Passanger::Passanger((string first_name, string last_name, string phone_num, int id_num){
+	fname = first_name;
+	lname = last_name; 
+	phone = phone_num;
+	id = id_num;
