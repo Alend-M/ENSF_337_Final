aea2a00bbb9d3938177fbd5bda7b4cca87f741b9
joshuamaher61 <144175356+joshuamaher61@users.noreply.github.com>
2023-11-28 15:31:38 -0700

    Update passenger.h

--- passenger.h
+++ passenger.h
@@ -1,14 +1,31 @@
+#include <string>
+
 #ifndef Passenger
 #define Passenger
 
+#include "Seat.h"
+#include <string>
+#include <vector>
+
+using namespace std;
+
 class Passenger{
+	public:
+	Passenger(string first_name, string last_name, string _num, int id_num, Seat seat);
+	void setFname(const char* fname);
+	void setLname(const char* lname);
+	void setId(const string* id);
+	void setPhone(const string* phone);
+	Passenger get_all() const {return Passenger}
 	
+	Passenger getPassenger() const;
 	
 	private:
-	char fname;
-	char lname;
-	//*Phone number make linked list
+	string fname;
+	string lname;
+	string phone;
 	int id;
+	Seat seat;
 	
 };
 
